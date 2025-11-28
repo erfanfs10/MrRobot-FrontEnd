447e11304a0eb31541cad97aa3c8b195908e9132
erfan <mahdi.farahmandsafa@gmail.com>
2025-11-28 22:12:46 +0330

    replace Farhang font with Kalameh font

--- public/fonts/font.js
+++ public/fonts/font.js
@@ -4,47 +4,47 @@ export const customFonts = localFont({
   variable: '--custom-font',
   src: [
     {
-      path: './web/woff2/Farhang2-Thin.woff2',
+      path: './kalameh/webfonts/woff2/KalamehWeb-Thin.woff2',
       weight: '100',
       style: 'normal',
     },
     {
-      path: './web/woff2/Farhang2-ExtraLight.woff2',
+      path: './kalameh/webfonts/woff2/KalamehWeb-ExtraLight.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: './web/woff2/Farhang2-Light.woff2',
+      path: './kalameh/webfonts/woff2/KalamehWeb-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: './web/woff2/Farhang2-Regular.woff2',
+      path: './kalameh/webfonts/woff2/KalamehWeb-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: './web/woff2/Farhang2-Medium.woff2',
+      path: './kalameh/webfonts/woff2/KalamehWeb-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: './web/woff2/Farhang2-DemiBold.woff2',
+      path: './kalameh/webfonts/woff2/KalamehWeb-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: './web/woff2/Farhang2-Bold.woff2',
+      path: './kalameh/webfonts/woff2/KalamehWeb-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: './web/woff2/Farhang2-ExtraBold.woff2',
+      path: './kalameh/webfonts/woff2/KalamehWeb-ExtraBold.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: './web/woff2/Farhang2-Black.woff2',
+      path: './kalameh/webfonts/woff2/KalamehWeb-Black.woff2',
       weight: '900',
       style: 'normal',
     },
