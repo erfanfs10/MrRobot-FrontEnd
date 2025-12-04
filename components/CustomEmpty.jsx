52f378e035689c6768c6ec02213185fe6b795cc7
erfan <mahdi.farahmandsafa@gmail.com>
2025-12-04 19:53:38 +0330

    udpate product types path

--- components/CustomEmpty.jsx
+++ components/CustomEmpty.jsx
@@ -18,7 +18,7 @@ const CustomEmpty = ({ children, title }) => {
       </EmptyHeader>
       <EmptyContent>
         <Button className="font-semibold py-6 px-2 text-md lg:text-xl hover:scale-110 duration-200 hover:cursor-pointer">
-          <Link href="/categories" rel="noopener noreferrer">
+          <Link href="/productTypes" rel="noopener noreferrer">
             نمایش دسته بندی ها
           </Link>
         </Button>
