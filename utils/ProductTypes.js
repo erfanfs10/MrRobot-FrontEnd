26bc22fcb3cb785554bdc2dbf8c4ca1ff003d9b5
erfan <mahdi.farahmandsafa@gmail.com>
2025-12-04 21:51:08 +0330

    create product types list to use it in navbar and sidebar

---
+++ utils/ProductTypes.js
@@ -0,0 +1,44 @@
+const productTypes = [
+  {
+    name: "قطعات کامپیوتر",
+    types: [
+      {name:"پردازنده", url:"CPU"},
+      {name:"کارت گرافیک", url:"GPU"},
+      {name:"مادربورد", url:"Motherboards"},
+      {name:"رم", url:"RAM"},
+      {name: "هارد", url:"Storage"},
+      {name:"منبع تغذیه", url:"Power Supply"},
+      {name:"فن کیس", url:"Case Fans"}
+    ]
+  },
+  {
+    name: "لوازم جانبی",
+    types: [
+      {name:"کیبورد", url:"Keyboard"},
+      {name: "ماوس", url:"Mouse"}
+    ]
+  },
+  {
+    name: "نمایشگر",
+    types: [
+      {name:"نمایشگر ساده", url:"Monitors"},
+      {name: "نمایشگر گیمینگ", url:"Gaming Monitors"}
+    ]
+  },
+  {
+    name: "کنسول بازی",
+    types: [
+      {name:"پلی استیشن", url:"PlayStation"},
+      {name: "اکس باکس", url:"Xbox"}
+    ]
+  },
+  {
+    name: "لپتاپ",
+    types: [
+      {name:"لپتاپ گیمینگ", url:"Gaming Laptops"},
+      {name: "لپتاپ مالتی مدیا", url:"Multimedia Laptops"}
+    ]
+  },
+]
+
+export default productTypes
