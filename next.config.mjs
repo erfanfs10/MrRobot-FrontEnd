e65d87306a53385b554114271d8708632221a904
erfan <mahdi.farahmandsafa@gmail.com>
2025-12-16 20:18:32 +0330

    add standalone config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+// Source - https://stackoverflow.com/a
+// Posted by Joshua Davis, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-12-16, License - CC BY-SA 4.0
+
+module.exports = {
+  output: 'standalone'
+}
 
 export default nextConfig;
