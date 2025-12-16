ed1bb6bb52ef844102f5ab66601a7c66ae25b21d
erfan <mahdi.farahmandsafa@gmail.com>
2025-12-16 21:57:04 +0330

    set api_url and static_files public

--- apiUrl.js
+++ apiUrl.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.API_URL
-export const STATIC_FILES = process.env.STATIC_FILES
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
+export const STATIC_FILES = process.env.NEXT_PUBLIC_STATIC_FILES
