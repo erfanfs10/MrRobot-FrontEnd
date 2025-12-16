26269215101c10a4896a665cd106e85010e43f75
erfan <mahdi.farahmandsafa@gmail.com>
2025-12-16 13:15:30 +0330

    get urls from .env

--- apiUrl.js
+++ apiUrl.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8080/"
-export const STATIC_FILES = "http://localhost:8080/static/"
+export const API_URL = process.env.API_URL
+export const STATIC_FILES = process.env.STATIC_FILES
