35df99c761fc9f9e2b417d31cbe03fb189c4d5cc
erfan <mahdi.farahmandsafa@gmail.com>
2025-12-20 22:43:42 +0330

    delete show all posts from home page

--- app/page.js
+++ app/page.js
@@ -28,7 +28,6 @@ export default async function Home() {
       />
       <RandomPosts
         title="پست های جدید"
-        buttonText="نمایش همه پست ها"
       />
       <FAQ/>
       {/* <ProductDetail /> */}
--- components/home/RandomPosts.jsx
+++ components/home/RandomPosts.jsx
@@ -1,6 +1,4 @@
 import CarouselPosts from "../post/CarouselPosts";
-import { Button } from "../ui/button";
-import Link from "next/link";
 import getData from "@/services/GetData";
 
 const RandomPosts = async ({ title, buttonText }) => {
@@ -10,12 +8,6 @@ const RandomPosts = async ({ title, buttonText }) => {
     <div className="flex flex-col gap-6 lg:gap-10 items-center pb-20">
       <h3 className="font-bold text-2xl lg:text-3xl">{title}</h3>
       <CarouselPosts posts={posts} />
-      <Button
-        asChild
-        className="font-semibold py-6 text-md lg:text-xl hover:scale-110 duration-200 hover:cursor-pointer"
-      >
-        <Link href="/posts">{buttonText}</Link>
-      </Button>
     </div>
   );
 };
